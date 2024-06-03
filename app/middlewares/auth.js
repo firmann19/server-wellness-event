7e50081adc86a04da8726df9262acb182435d137
Firman Ramadhan <firman19ramadhan@gmail.com>
2024-06-03 14:53:29 +0700

    push lagi

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -35,7 +35,7 @@ const authenticateUser = async (req, res, next) => {
 
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role.role)) {
+    if (!roles.includes(req.user.role)) {
       throw new UnauthorizedError("Unauthorized to access this route");
     }
     next();
